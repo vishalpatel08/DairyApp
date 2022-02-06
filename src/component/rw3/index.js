174dde50882ac3e0b043c7c6113a26bb06af0a85
vishalpatel08 <60808575+vishalpatel08@users.noreply.github.com>
2022-02-06 19:18:54 +0530

    Transfer

--- src/component/rw3/index.js
+++ src/component/rw3/index.js
@@ -3,14 +3,14 @@ import './Project1.css'
 import {Link} from 'react-router-dom'
 
 const Frontpage = () => {
+
     return(
+        <div className='f-div1'>
             <div className='f-div'>
-            {/* 
-            <Link to='/removeuser'> <button id='p-button'> Remove user </button> </Link>
-            <Link to='/checkuserdata'><button id='p-button'> Check user data </button></Link> */}
-            <Link to='/userlist' > <button id='p-button'> User List </button> </Link>
-            <Link to='/adduser'><button id='p-button'> Add New User </button></Link>
+                <div className='flink1'><Link to='/userlist' > <button id='p-button'> User List </button> </Link></div>
+                <div className='flink2'><Link to='/adduser'><button id='p-button'> Add New User </button></Link></div>
             </div>
+        </div>
         )
 }
 
